40f10d933a704374c1663c69bf6dd7a68804c2ac
Haridu Senadeera <hsenadeera@solstice.com>
2018-08-27 14:54:09 -0500

    fixed indentation

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,52 +1,52 @@
 import {
-    AUTHENTICATION_REQUEST,
-    AUTHENTICATION_SUCCESS,
-    AUTHENTICATION_FAILURE,
-    PERFORM_LOGOUT
-  } from '../constants/actions';
+  AUTHENTICATION_REQUEST,
+  AUTHENTICATION_SUCCESS,
+  AUTHENTICATION_FAILURE,
+  PERFORM_LOGOUT
+} from '../constants/actions';
 
 const initialState = {
-    isFetching: false,
-    isAuthenticated: false,
-    error: null
+  isFetching: false,
+  isAuthenticated: false,
+  error: null
 }
 
 export default function (state = initialState, action) {
-    switch (action.type) {
-        case AUTHENTICATION_REQUEST:
-            return {
-                ...state,
-                isFetching: action.payload.isFetching,
-                isAuthenticated: action.payload.isAuthenticated,
-                error: null
-            }
-        case AUTHENTICATION_SUCCESS:
-            return {
-                ...state,
-                isFetching: action.payload.isFetching,
-                isAuthenticated: action.payload.isAuthenticated,
-                user: {
-                    email: action.payload.email,
-                    username: action.payload.username,
-                    firstName: action.payload.firstName,
-                    lastName: action.payload.lastName
-                }
-            }
-        case AUTHENTICATION_FAILURE:
-            return {
-                ...state,
-                isFetching: action.payload.isFetching,
-                isAuthenticated: action.payload.isAuthenticated,
-                error: action.payload.error
-            }
-        case PERFORM_LOGOUT:
-            return {
-                ...state,
-                isFetching: action.payload.isFetching,
-                isAuthenticated: action.payload.isAuthenticated,
-                user: null
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case AUTHENTICATION_REQUEST:
+      return {
+        ...state,
+        isFetching: action.payload.isFetching,
+        isAuthenticated: action.payload.isAuthenticated,
+        error: null
+      }
+    case AUTHENTICATION_SUCCESS:
+      return {
+        ...state,
+        isFetching: action.payload.isFetching,
+        isAuthenticated: action.payload.isAuthenticated,
+        user: {
+          email: action.payload.email,
+          username: action.payload.username,
+          firstName: action.payload.firstName,
+          lastName: action.payload.lastName
+        }
+      }
+    case AUTHENTICATION_FAILURE:
+      return {
+        ...state,
+        isFetching: action.payload.isFetching,
+        isAuthenticated: action.payload.isAuthenticated,
+        error: action.payload.error
+      }
+    case PERFORM_LOGOUT:
+      return {
+        ...state,
+        isFetching: action.payload.isFetching,
+        isAuthenticated: action.payload.isAuthenticated,
+        user: null
+      }
+    default:
+      return state;
+  }
 }
